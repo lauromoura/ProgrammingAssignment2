2fee41c824151b9af0d17711df5d07d0f6f746ac
Lauro Moura <lauromoura@gmail.com>
2014-12-21 08:24:04 -0300

    Adding cachematrix implementations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache inverse matrix calculation through scoping management
 
-## Write a short comment describing this function
+## Creates a cache wrapper for a matrix object x, allowing to save
+## the inverse data in the inv variable
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## Receives a CacheMatrix object x created from the method above and,
+## if already cached, returns its inverse. If not, calculates the inverse,
+## stores it in x and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinv(inv)
+    inv
 }
